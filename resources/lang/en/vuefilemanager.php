23bbae8a7419a3661410d972cf4da815297f7581
Peter Papp <peterpapp@makingcg.com>
2020-08-11 08:46:03 +0200

    meta description fix

--- resources/lang/en/vuefilemanager.php
+++ resources/lang/en/vuefilemanager.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_description'         => 'Make your own Private Cloud with VueFileManager client powered by Laravel and Vue',
+    'app_description'         => 'Your self-hosted storage cloud software powered by Laravel and Vue',
     'user_not_fount'          => 'We can\'t find a user with that e-mail address.',
     'incorrect_password'      => 'Sorry, your password is incorrect.',
     'time'                    => '%d. %B. %Y at %H:%M',
