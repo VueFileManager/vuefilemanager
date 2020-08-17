47faadef51a4b84d1b5374dd8300e258867796fa
Peter Papp <peterpapp@makingcg.com>
2020-08-17 08:42:22 +0200

    strip non-asci characters from filename

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -115,7 +115,8 @@ const Helpers = {
                 let formData = new FormData(),
                     uploadedSize = 0,
                     isNotGeneralError = true,
-                    filename = Array(16).fill(0).map(x => Math.random().toString(36).charAt(2)).join('') + '-' + file.name + '.part'
+                    striped_name = file.name.replace(/[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g, ''),
+                    filename = Array(16).fill(0).map(x => Math.random().toString(36).charAt(2)).join('') + '-' + striped_name + '.part'
 
                 do {
                     let isLast = chunks.length === 1,
