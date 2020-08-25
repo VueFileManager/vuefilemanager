73a728e60629913abddfdb50dc8119e833effdae
Peter Papp <peterpapp@makingcg.com>
2020-08-25 12:37:09 +0200

    change vuefilemanager version

--- config/vuefilemanager.php
+++ config/vuefilemanager.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version'    => '1.7.7',
+    'version'    => '1.7.8',
 
     // Define size of chunk uploaded by MB. E.g. integer 128 means chunk size will be 128MB.
     'chunk_size' => env('CHUNK_SIZE', '128'),
