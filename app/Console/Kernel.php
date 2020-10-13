12d6b0bf57a4445b52b8fe4b08b3cdb0f0aa9100
Peter Papp <peterpapp@makingcg.com>
2020-10-13 17:09:05 +0200

    scheduler changed from hourly to every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             $this->delete_expired_shared_links();
-        })->hourly();
+        })->everyMinute();
     }
 
     /**
