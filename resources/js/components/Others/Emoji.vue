6c3630085e526de8a30520124996304cc64c8831
Milos Holba <milosholba78@gmail.com>
2021-02-09 20:42:48 +0100

    test for iOS emojis

--- resources/js/components/Others/Emoji.vue
+++ resources/js/components/Others/Emoji.vue
@@ -1,25 +1,57 @@
 <template>
-    <div v-show="transferEmoji" :style="{width: `${size}px`, height: `${size}px`}"  v-html="transferEmoji"/>
+    <div>
+        <div v-if="transferEmoji && !isIos" :style="{width: `${size}px`, height: `${size}px`}"  v-html="transferEmoji"/>
+        <span v-if="transferEmoji && isIos" :style="{fontSize: `${size}px`}">{{transferEmoji}}</span>
+    </div>
+    
 </template>
 
 <script>
 import twemoji from 'twemoji'
-    export default {
-        name: 'Emoji',
-        props: ['emoji', 'size'],
-        computed: {
-            transferEmoji () {
-                
-                // Transfer single emoji to twemoji
-                return twemoji.parse(this.emoji.char, {
-                    folder: 'svg',
-                    ext: '.svg',
-                    attributes: () => ({
-                        loading: 'lazy',
+export default {
+    name: 'Emoji',
+    props: ['emoji', 'size'],
+    computed: {
+        transferEmoji () {
+            
+            // Transfer single emoji to twemoji
+            return !this.isIos 
+                   ? twemoji.parse(this.emoji.char, {
+                        folder: 'svg',
+                        ext: '.svg',
+                        attributes: () => ({
+                            loading: 'lazy',
+                        }) 
                     }) 
-                })
-            }
+                    : this.emoji.char
         },
+    },
+    data () {
+        return {
+            isIos: false
+        }
+    },
+    created () {
+       
+        const toMatch = [
+                /iPhone/i,
+                /iPad/i,
+                /iPod/i,
+                /iOS/i,
+                /macOS/i,
+                /Macintosh/i
+            ]
+        this.isIos = toMatch.some(toMatchItem => {
+            return navigator.userAgent.match(toMatchItem)
+        })
     }
+}
 </script>
+
+<style lang="scss" scoped>
+@import "@assets/vue-file-manager/_inapp-forms.scss";
+@import '@assets/vue-file-manager/_forms';
+
+
+</style>
 
