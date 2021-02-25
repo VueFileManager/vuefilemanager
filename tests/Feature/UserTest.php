ac38d35ae11ab654058949bd2f84a702f0541f8f
Peter Papp <peterpapp@makingcg.com>
2021-02-25 17:16:09 +0100

    Migration refactoring

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
