5227a5fb99f784334c151eded507c5d2c93271d8
Peter Papp <peterpapp@makingcg.com>
2021-02-26 18:17:33 +0100

    - test skelet for browsing items

---
+++ tests/Feature/BrowseTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BrowseTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    public function it_get_navigator_tree()
+    {
+
+    }
+
+    public function it_get_folder_files()
+    {
+
+    }
+
+    public function it_get_searched_file()
+    {
+
+    }
+
+    public function it_get_searched_folder()
+    {
+
+    }
+
+    public function it_get_recent_files()
+    {
+
+    }
+
+    public function it_get_trash()
+    {
+
+    }
+
+    public function it_get_shared_files()
+    {
+
+    }
+
+    public function it_get_participant_uploads()
+    {
+
+    }
+}
