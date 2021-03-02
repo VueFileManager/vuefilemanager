617ba2a6a88e4360758641b9c2b3684914dcd864
Peter Papp <peterpapp@makingcg.com>
2021-03-02 17:05:57 +0100

    - added it_delete_zips_older_than_one_day test - refactored scheduler tasks into SchedulerService.php

---
+++ tests/Feature/SchedulerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\File;
+use App\Models\Zip;
+use App\Services\SchedulerService;
+use App\Services\SetupService;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
+use Storage;
+use Tests\TestCase;
+
+class SchedulerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setup = app()->make(SetupService::class);
+        $this->scheduler = app()->make(SchedulerService::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_delete_zips_older_than_one_day()
+    {
+        Storage::fake('local');
+
+        $this->setup->create_directories();
+
+        $file = UploadedFile::fake()
+            ->create('archive.zip', 2000, 'application/zip');
+
+        Storage::putFileAs('zip', $file, 'EHWKcuvKzA4Gv29v-archive.zip');
+
+        $zip = Zip::factory(Zip::class)->create([
+            'basename' => 'EHWKcuvKzA4Gv29v-archive.zip',
+            'created_at' => Carbon::now()->subDay(),
+        ]);
+
+        $this->scheduler->delete_old_zips();
+
+        $this->assertDatabaseMissing('zips', [
+            'id' => $zip->id
+        ]);
+
+        Storage::disk('local')
+            ->assertMissing('zip/EHWKcuvKzA4Gv29v-archive.zip');
+    }
+}
