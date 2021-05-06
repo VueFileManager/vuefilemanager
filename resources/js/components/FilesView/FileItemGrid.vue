3c8d6ac862868a59e5589e293c79975381f8ff1b
Peter Papp <peterpapp@makingcg.com>
2021-05-06 10:23:10 +0200

    Fixed renaming item in desktop

--- resources/js/components/FilesView/FileItemGrid.vue
+++ resources/js/components/FilesView/FileItemGrid.vue
@@ -176,8 +176,10 @@ export default {
 
             if (!this.$isMobile()) {
 
-                // After click deselect new folder rename input
-                document.getSelection().removeAllRanges();
+				// After click deselect new folder rename input
+				if (document.getSelection().toString().length) {
+					document.getSelection().removeAllRanges();
+				}
 
                 if (e.ctrlKey || e.metaKey && !e.shiftKey) {
                     // Click + Ctrl
