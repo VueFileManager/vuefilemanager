0df72b180359998824ae0ae203814d52f0a3207d
Milos Holba <milosholba78@gmail.com>
2021-05-26 13:08:31 +0200

    change text in the successfully verified page

--- resources/js/views/Auth/SuccessfullyEmailVerified.vue
+++ resources/js/views/Auth/SuccessfullyEmailVerified.vue
@@ -4,8 +4,8 @@
             <img v-if="config.app_logo" class="logo" :src="$getImage(config.app_logo)" :alt="config.app_name">
             <b v-if="! config.app_logo" class="auth-logo-text">{{ config.app_name }}</b>
           
-            <h1>{{ $t('page_login.title') }}</h1>
-            <h2>{{ $t('page_email_successfully_verified') }}</h2>
+            <h1>{{ $t('page_email_successfully_verified.title') }}</h1>
+            <h2>{{ $t('page_email_successfully_verified.subtitle') }}</h2>
             
             <a href="/sign-in">
                 <AuthButton icon="chevron-right" :text="$t('page_sign_in.button_log_in')"/>
