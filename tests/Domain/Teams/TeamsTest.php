fdd9c5a59167bcccf956097373132168bde86e6b
Peter Papp <peterpapp@makingcg.com>
2021-08-24 09:12:57 +0200

    team folders test skelet

---
+++ tests/Domain/Teams/TeamsTest.php
@@ -0,0 +1,90 @@
+<?php
+
+
+namespace Tests\Domain\Teams;
+
+
+use Tests\TestCase;
+
+class TeamsTest extends TestCase
+{
+    /**
+     *
+     */
+    public function it_create_team_folder()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_convert_team_folder()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_add_member_into_team_folder()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function member_accept_team_folder_invite()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function member_reject_team_folder_invite()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_remove_member_from_team_folder()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_dissolve_team_folder()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_move_items_into_team_folder()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_get_all_team_folders()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function it_get_team_folders_shared_with_another_user()
+    {
+
+    }
+}
