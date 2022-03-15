556f6d0813670f130a282c7cf63ffeb5222a0647
Čarodej <peterpapp@makingcg.com>
2022-03-15 09:08:54 +0100

    fixed uploaded image default rotation

--- src/Domain/Files/Actions/GenerateImageThumbnailAction.php
+++ src/Domain/Files/Actions/GenerateImageThumbnailAction.php
@@ -13,20 +13,20 @@ class GenerateImageThumbnailAction
     {
         $localDisk = Storage::disk('local');
 
-        // Get image from disk
-        $image = $localDisk->get("temp/$userId/$fileName");
-
         // Get image width
         $imageWidth = getimagesize($localDisk->path("temp/$userId/$fileName"))[0];
 
         collect(config("vuefilemanager.image_sizes.$execution"))
-            ->each(function ($size) use ($image, $userId, $fileName, $imageWidth) {
+            ->each(function ($size) use ($userId, $fileName, $imageWidth) {
                 if ($imageWidth > $size['size']) {
                     // Create intervention image
-                    $intervention = Image::make($image)->orientate();
+                    $intervention = Image::make(config('filesystems.disks.local.root') . "/temp/$userId/$fileName")
+                        ->orientate();
 
                     // Generate thumbnail
-                    $intervention->resize($size['size'], null, fn ($constraint) => $constraint->aspectRatio())->stream();
+                    $intervention
+                        ->resize($size['size'], null, fn ($constraint) => $constraint->aspectRatio())
+                        ->stream();
 
                     // Store thumbnail to disk
                     Storage::put("files/$userId/{$size['name']}-$fileName", $intervention);
