d1d58711c52eb0cd4456312a8fd8bd060c5ea6ba
Čarodej <peterpapp@makingcg.com>
2022-03-30 09:56:14 +0200

    disabled moving and deleting folders from root of TeamFolders

--- resources/js/components/FilesView/InfoSidebar.vue
+++ resources/js/components/FilesView/InfoSidebar.vue
@@ -1,7 +1,7 @@
 <template>
     <div
-        class="hidden w-[300px] shrink-0 overflow-y-auto overflow-x-hidden px-2.5 pt-2 lg:block xl:w-[320px] 2xl:w-[360px]"
-    >
+		class="hidden w-[300px] shrink-0 overflow-y-auto overflow-x-hidden px-2.5 pt-2 lg:block xl:w-[320px] 2xl:w-[360px]"
+	>
         <!--Is empty clipboard-->
         <div v-if="isEmpty" class="flex h-full items-center justify-center">
             <div class="text-center">
@@ -12,55 +12,55 @@
             </div>
         </div>
 
-        <!--Multiple item selection-->
+		<!--Multiple item selection-->
         <TitlePreview
-            v-if="!isSingleFile && !isEmpty"
-            class="mb-6"
-            icon="check-square"
-            :title="$t('selected_multiple')"
-            :subtitle="this.clipboard.length + ' ' + $tc('items', this.clipboard.length)"
-        />
+			v-if="!isSingleFile && !isEmpty"
+			class="mb-6"
+			icon="check-square"
+			:title="$t('selected_multiple')"
+			:subtitle="this.clipboard.length + ' ' + $tc('items', this.clipboard.length)"
+		/>
 
-        <!--Single file preview-->
+		<!--Single file preview-->
         <div v-if="isSingleFile && !isEmpty">
             <FilePreviewDetail />
 
             <TitlePreview
-                class="mb-6"
-                :icon="clipboard[0].data.type"
-                :title="clipboard[0].data.attributes.name"
-                :subtitle="clipboard[0].data.attributes.mimetype"
-            />
+				class="mb-6"
+				:icon="clipboard[0].data.type"
+				:title="clipboard[0].data.attributes.name"
+				:subtitle="clipboard[0].data.attributes.mimetype"
+			/>
 
-            <!--Filesize-->
+			<!--Filesize-->
             <ListInfoItem
-                v-if="singleFile.data.attributes.filesize"
-                :title="$t('size')"
-                :content="singleFile.data.attributes.filesize"
-            />
+				v-if="singleFile.data.attributes.filesize"
+				:title="$t('size')"
+				:content="singleFile.data.attributes.filesize"
+			/>
 
-            <!--Created At-->
+			<!--Created At-->
             <ListInfoItem :title="$t('created_at')" :content="singleFile.data.attributes.created_at" />
 
-            <!--Location-->
-            <ListInfoItem v-if="$checkPermission(['master'])" :title="$t('where')">
+			<!--Location-->
+            <ListInfoItem v-if="$checkPermission(['master']) && !isTeamsHomepage" :title="$t('where')">
                 <div @click="$moveFileOrFolder(singleFile)" class="flex cursor-pointer items-center">
                     <b class="inline-block text-sm font-bold">
                         {{
-                            singleFile.data.relationships.parent
-                                ? singleFile.data.relationships.parent.data.attributes.name
-                                : $getCurrentLocationName()
-                        }}
+							singleFile.data.relationships.parent
+								? singleFile.data.relationships.parent.data.attributes.name
+								: $getCurrentLocationName()
+						}}
                     </b>
                     <Edit2Icon size="10" class="ml-2" />
                 </div>
             </ListInfoItem>
 
-            <!--Shared-->
+			<!--Shared-->
             <ListInfoItem
-                v-if="$checkPermission('master') && singleFile.data.relationships.shared"
-                :title="$t('shared')"
-            >
+				v-if="$checkPermission('master') && singleFile.data.relationships.shared"
+				:title="$t('shared')"
+			>
                 <div @click="$shareFileOrFolder(singleFile)" class="mb-2 flex cursor-pointer items-center">
                     <span class="inline-block text-sm font-bold">
                         {{ sharedInfo }}
@@ -69,22 +69,22 @@
                 </div>
                 <div class="flex w-full items-center">
                     <lock-icon
-                        v-if="isLocked"
-                        @click="$shareFileOrFolder(singleFile)"
-                        size="17"
-                        class="hover-text-theme vue-feather cursor-pointer"
-                    />
+						v-if="isLocked"
+						@click="$shareFileOrFolder(singleFile)"
+						size="17"
+						class="hover-text-theme vue-feather cursor-pointer"
+					/>
                     <unlock-icon
-                        v-if="!isLocked"
-                        @click="$shareFileOrFolder(singleFile)"
-                        size="17"
-                        class="hover-text-theme vue-feather cursor-pointer"
-                    />
+						v-if="!isLocked"
+						@click="$shareFileOrFolder(singleFile)"
+						size="17"
+						class="hover-text-theme vue-feather cursor-pointer"
+					/>
                     <CopyShareLink :item="singleFile" size="small" class="w-full pl-2.5" />
                 </div>
             </ListInfoItem>
 
-            <!--Author-->
+			<!--Author-->
             <ListInfoItem v-if="canShowAuthor" :title="$t('author')">
                 <div class="mt-1.5 flex items-center">
                     <MemberAvatar :size="32" :member="singleFile.data.relationships.creator" />
@@ -94,7 +94,7 @@
                 </div>
             </ListInfoItem>
 
-            <!--Metadata-->
+			<!--Metadata-->
             <ListInfoItem v-if="canShowMetaData" :title="$t('meta_data')">
                 <ImageMetaData />
             </ListInfoItem>
@@ -105,64 +105,67 @@
 <script>
 import FilePreviewDetail from '../Others/FilePreviewDetail'
 import CopyShareLink from '../Others/Forms/CopyShareLink'
-import { Edit2Icon, LockIcon, UnlockIcon, EyeOffIcon } from 'vue-feather-icons'
+import {Edit2Icon, LockIcon, UnlockIcon, EyeOffIcon} from 'vue-feather-icons'
 import ImageMetaData from './ImageMetaData'
 import TitlePreview from './TitlePreview'
 import TeamMembersPreview from '../Teams/Components/TeamMembersPreview'
 import ListInfoItem from '../Others/ListInfoItem'
 import MemberAvatar from './MemberAvatar'
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 
-	export default {
-		name: 'InfoSidebar',
-		components: {
-			TeamMembersPreview,
-			FilePreviewDetail,
-			ImageMetaData,
-			CopyShareLink,
-			MemberAvatar,
-			TitlePreview,
-			ListInfoItem,
-			UnlockIcon,
-			EyeOffIcon,
-			Edit2Icon,
-			LockIcon,
+export default {
+	name: 'InfoSidebar',
+	components: {
+		TeamMembersPreview,
+		FilePreviewDetail,
+		ImageMetaData,
+		CopyShareLink,
+		MemberAvatar,
+		TitlePreview,
+		ListInfoItem,
+		UnlockIcon,
+		EyeOffIcon,
+		Edit2Icon,
+		LockIcon,
+	},
+	computed: {
+		...mapGetters([
+			'permissionOptions',
+			'clipboard',
+			'user',
+		]),
+		isEmpty() {
+			return this.clipboard.length === 0
 		},
-		computed: {
-			...mapGetters([
-				'permissionOptions',
-				'clipboard',
-				'user',
-			]),
-			isEmpty() {
-				return this.clipboard.length === 0
-			},
-			isSingleFile() {
-				return this.clipboard.length === 1
-			},
-			singleFile() {
-				return this.clipboard[0]
-			},
-			canShowMetaData() {
-				return this.clipboard[0].data.relationships.exif
-			},
-			isLocked() {
-				return this.clipboard[0].data.relationships.shared.protected
-			},
-			sharedInfo() {
-				let title = this.permissionOptions.find(option => {
-					return option.value === this.clipboard[0].data.relationships.shared.permission
-				})
+		isSingleFile() {
+			return this.clipboard.length === 1
+		},
+		singleFile() {
+			return this.clipboard[0]
+		},
+		canShowMetaData() {
+			return this.clipboard[0].data.relationships.exif
+		},
+		isLocked() {
+			return this.clipboard[0].data.relationships.shared.protected
+		},
+		sharedInfo() {
+			let title = this.permissionOptions.find(option => {
+				return option.value === this.clipboard[0].data.relationships.shared.permission
+			})
 
-            return title ? this.$t(title.label) : this.$t('can_download_file')
-        },
-        canShowAuthor() {
-            return (
-                this.$isThisRoute(this.$route, ['SharedWithMe', 'TeamFolders']) &&
-                this.clipboard[0].data.type !== 'folder' &&
-                this.user.data.id !== this.clipboard[0].data.relationships.creator.data.id
-            )
-        },
-    },
+			return title ? this.$t(title.label) : this.$t('can_download_file')
+		},
+		canShowAuthor() {
+			return (
+				this.$isThisRoute(this.$route, ['SharedWithMe', 'TeamFolders']) &&
+				this.clipboard[0].data.type !== 'folder' &&
+				this.user.data.id !== this.clipboard[0].data.relationships.creator.data.id
+			)
+		},
+		isTeamsHomepage() {
+			return this.$isThisRoute(this.$route, ['TeamFolders', 'SharedWithMe']) && !this.$route.params.id
+		},
+	},
 }
 </script>
