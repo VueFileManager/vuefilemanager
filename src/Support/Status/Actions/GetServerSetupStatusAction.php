a3805b4bff7e982641c4c53b0c6cc27f697dc2eb
Čarodej <peterpapp@makingcg.com>
2022-04-02 20:31:18 +0200

    removed pcntl

--- src/Support/Status/Actions/GetServerSetupStatusAction.php
+++ src/Support/Status/Actions/GetServerSetupStatusAction.php
@@ -31,7 +31,6 @@ class GetServerSetupStatusAction
                 'sqlite3'   => extension_loaded('sqlite3'),
                 'bcmath'    => extension_loaded('bcmath'),
                 'ctype'     => extension_loaded('ctype'),
-                'pcntl'     => extension_loaded('pcntl'),
                 'json'      => extension_loaded('json'),
                 'exif'      => extension_loaded('exif'),
                 'intl'      => extension_loaded('intl'),
