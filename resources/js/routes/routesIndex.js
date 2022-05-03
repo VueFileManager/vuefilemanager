37cc94aedaf9492fdea97d5c994c28fde6f253f6
Čarodej <peterpapp@makingcg.com>
2022-05-03 10:48:31 +0200

    added demo page with button redirection

--- resources/js/routes/routesIndex.js
+++ resources/js/routes/routesIndex.js
@@ -23,6 +23,14 @@ const routesIndex = [
             requiresAuth: false,
         },
     },
+    {
+        name: 'Demo',
+        path: '/demo',
+        component: () => import(/* webpackChunkName: "chunks/demo" */ '../views/Demo'),
+        meta: {
+            requiresAuth: false,
+        },
+    },
 ]
 
 export default routesIndex
---
+++ resources/js/views/Demo.vue
@@ -0,0 +1,34 @@
+<template>
+    <AuthContentWrapper ref="auth" class="h-screen">
+        <AuthContent :visible="true">
+            <Headline
+				title="You Are Nearly Here!"
+				description="Just click on the button below."
+			/>
+
+			<a :href="`${$store.getters.config.host}/sign-in`" target="_blank">
+				<AuthButton
+					class="mb-12 w-full justify-center md:w-min"
+					icon="chevron-right"
+					text="Visit Demo"
+				/>
+			</a>
+        </AuthContent>
+    </AuthContentWrapper>
+</template>
+<script>
+import AuthContentWrapper from '../components/Layout/AuthPages/AuthContentWrapper'
+import AuthContent from '../components/Layout/AuthPages/AuthContent'
+import AuthButton from '../components/UI/Buttons/AuthButton'
+import Headline from '../components/UI/Labels/LogoHeadline'
+
+export default {
+    name: 'Demo',
+    components: {
+        AuthContentWrapper,
+        AuthContent,
+        AuthButton,
+        Headline,
+    },
+}
+</script>
