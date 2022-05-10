1ed8a119c608350845714cacae0cfa5181fad737
Čarodej <peterpapp@makingcg.com>
2022-05-10 16:15:10 +0200

    build

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -118,11 +118,6 @@ export default {
                 this.isLoaded = true
             })
         }
-
-		// Go to sign in page if homepage is disabled
-		if (!this.$root.$data.config.allowHomepage && window.location.pathname === '/') {
-			this.$router.push({ name: 'SignIn' })
-		}
     },
     created() {
         if (this.$isWindows()) {
